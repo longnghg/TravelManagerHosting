c0106bceafcef47014ec05ba347304e3ac49a8a1
Long.nh <ng.hglong102@gmail.com>
2022-10-01 11:09:00 +0700

    Add framwork EF core

---
+++ Travel.Context/Models/Notification/NotificationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel.Context.Models.Notification
+{
+    public class NotificationContext : DbContext
+    {
+        public NotificationContext()
+        {
+        }
+
+        public NotificationContext(DbContextOptions<NotificationContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Travel.Context/Models/Travel/TravelContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel.Context.Models.Travel
+{
+    public class TravelContext : DbContext
+    {
+        public TravelContext()
+        {
+        }
+
+        public TravelContext(DbContextOptions<TravelContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
