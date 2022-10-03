1e5e509b3aa3198ed3fa67907fae509f91be4f0e
KIET\Administrator <phuongkiet850@gmail.com>
2022-10-03 12:08:00 +0700

    Thêm ViewModel Responsive

---
+++ Travel.Shared/ViewModels/Responsive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel.Shared.ViewModels
+{
+    public class Responsive : Pagination
+    {
+        private int status;
+        private string content;
+        private Notification notification;
+
+        public int Status { get => status; set => status = value; }
+        public string Content { get => content; set => content = value; }
+        public Notification Notification { get => notification; set => notification = value; }
+    }
+}
