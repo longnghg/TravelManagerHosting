b5a2b59a6b9468d5a26c1532ea58d885f92705cf
Dev_HuyHoa <driverhuyoa@gmail.com>
2022-10-04 13:16:37 +0700

    no message

--- Travel.Context/Models/Travel/TravelContext.cs
+++ Travel.Context/Models/Travel/TravelContext.cs
@@ -23,6 +23,7 @@ namespace Travel.Context.Models.Travel
         public DbSet<Province> Provinces { get; set; }
         public DbSet<District> Districts { get; set; }
         public DbSet<Ward> Wards { get; set; }
+        public DbSet<Customer> Customers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Payment>(entity =>
@@ -30,6 +31,10 @@ namespace Travel.Context.Models.Travel
                 entity.HasOne(e => e.Tourbooking)
                 .WithMany(d => d.Payment)
                 .HasForeignKey(e => e.IdTourBooking);
+                entity.Property(e => e.Id).HasMaxLength(30);
+                entity.Property(e => e.Name).HasMaxLength(100);
+                entity.Property(e => e.Type).HasMaxLength(30);
+                entity.Property(e => e.IdTourBooking).HasMaxLength(30);
             });
 
             // tourbooking
@@ -39,10 +44,34 @@ namespace Travel.Context.Models.Travel
                 .WithOne(e => e.Tourbooking)                             
                 .HasForeignKey<TourbookingDetails>(e => e.IdTourBooking);
 
-                entity.Property(e => e.Email).HasDefaultValue("");
+                entity.Property(e => e.Email).HasMaxLength(100);
                 entity.Property(e => e.Address).HasMaxLength(100);
                 entity.Property(e => e.Phone1).HasMaxLength(14);
-                //entity.Property(e => e.TourbookingDetails).IsRequired(true);
+                entity.Property(e => e.Pincode).HasMaxLength(10);
+                entity.Property(e => e.Id).HasMaxLength(30);
+                entity.Property(e => e.CustomerName).HasMaxLength(100);
+                entity.Property(e => e.ContactName).HasMaxLength(100);
+                entity.Property(e => e.VoucherCode).HasMaxLength(10);
+                entity.Property(e => e.ModifyBy).HasMaxLength(100);
+                entity.Property(e => e.BookingNo).HasMaxLength(30);
+
+
+
+                entity.Property(e => e.Email).IsRequired(true);
+                entity.Property(e => e.Phone1).IsRequired(true);              
+            });
+
+            modelBuilder.Entity<TourbookingDetails>(entity =>
+            {
+                entity.Property(e => e.Pincode).HasMaxLength(10);
+                entity.Property(e => e.IdTourBooking).HasMaxLength(30);
+            });
+
+            modelBuilder.Entity<Province>(entity =>
+            {
+                entity.Property(e => e.Name).HasMaxLength(30);
+                entity.Property(e => e.Name).IsRequired(true);
+
             });
 
             modelBuilder.Entity<District>(entity =>
@@ -50,7 +79,9 @@ namespace Travel.Context.Models.Travel
                 entity.HasOne(e => e.Province)
                 .WithMany(d => d.Districts)
                 .HasForeignKey(e => e.IdProvice);
-                entity.Property(e => e.Name).HasMaxLength(30);
+                entity.Property(e => e.Name).HasMaxLength(100);
+                entity.Property(e => e.Name).IsRequired(true);
+
             });
 
             modelBuilder.Entity<Ward>(entity =>
@@ -59,6 +90,23 @@ namespace Travel.Context.Models.Travel
                 .WithMany(e => e.Ward)
                 .HasForeignKey(e => e.IdDistrict);
                 entity.Property(e => e.Name).HasMaxLength(30);
+                entity.Property(e => e.Name).IsRequired(true);
+            
+            });
+            
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.Name).HasMaxLength(100);
+                entity.Property(e => e.Address).HasMaxLength(100);
+                entity.Property(e => e.Phone).HasMaxLength(14);
+                entity.Property(e => e.Password).HasMaxLength(30);
+                entity.Property(e => e.FbToken).HasMaxLength(300);
+                entity.Property(e => e.AccessToken).HasMaxLength(300);
+                entity.Property(e => e.GoogleToken).HasMaxLength(300);
+                entity.Property(e => e.Name).IsRequired(true);
+                entity.Property(e => e.Email).IsRequired(true);
+                entity.Property(e => e.Phone).IsRequired(true);
+                
             });
         }
         
