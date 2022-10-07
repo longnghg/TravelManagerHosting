63eb509353bce0a82ba975c85a94c031160b1889
H1ieenrrr <hienltps16180@fpt.edu.vn>
2022-10-07 19:48:09 +0700

    ViewModel View Tour, CostTour, Schedule

---
+++ Travel.Shared/ViewModels/Travel/CostTourViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel.Shared.ViewModels.Travel
+{
+    public class CostTourViewModel
+    {
+        private Guid id;
+        private string idTourDetail;
+        private float breakfast;
+        private float water;
+        private float feeGas;
+        private float distance;
+        private float sellCost;
+        private float depreciation;
+        private float otherPrice;
+        private float tolls;
+        private int cusExpected;
+        private float insuranceFee;
+        private bool isHoliday;
+        private float totalCostTour;
+
+        private Guid idHotel;
+        private string nameHotel;
+        private float priceHotel;
+
+        private Guid idRestaurant;
+        private string nameRestaurant;
+        private float priceRestaurant;
+
+        private Guid idPlace;
+        private string namePlace;
+        private float priceTicketPlace;
+
+
+        public Guid Id { get => id; set => id = value; }
+        public string IdTourDetail { get => idTourDetail; set => idTourDetail = value; }
+        public float Breakfast { get => breakfast; set => breakfast = value; }
+        public float Water { get => water; set => water = value; }
+        public float FeeGas { get => feeGas; set => feeGas = value; }
+        public float Distance { get => distance; set => distance = value; }
+        public float SellCost { get => sellCost; set => sellCost = value; }
+        public float Depreciation { get => depreciation; set => depreciation = value; }
+        public float OtherPrice { get => otherPrice; set => otherPrice = value; }
+        public float Tolls { get => tolls; set => tolls = value; }
+        public int CusExpected { get => cusExpected; set => cusExpected = value; }
+        public float InsuranceFee { get => insuranceFee; set => insuranceFee = value; }
+        public bool IsHoliday { get => isHoliday; set => isHoliday = value; }
+        public float TotalCostTour { get => totalCostTour; set => totalCostTour = value; }
+        public Guid IdHotel { get => idHotel; set => idHotel = value; }
+        public string NameHotel { get => nameHotel; set => nameHotel = value; }
+        public float PriceHotel { get => priceHotel; set => priceHotel = value; }
+        public Guid IdRestaurant { get => idRestaurant; set => idRestaurant = value; }
+        public string NameRestaurant { get => nameRestaurant; set => nameRestaurant = value; }
+        public float PriceRestaurant { get => priceRestaurant; set => priceRestaurant = value; }
+        public Guid IdPlace { get => idPlace; set => idPlace = value; }
+        public string NamePlace { get => namePlace; set => namePlace = value; }
+        public float PriceTicketPlace { get => priceTicketPlace; set => priceTicketPlace = value; }
+    }
+}
---
+++ Travel.Shared/ViewModels/Travel/ScheduleViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travel.Context.Models;
+
+namespace Travel.Shared.ViewModels.Travel
+{
+    public class ScheduleViewModel
+    {
+        private string id;
+        private long departureDate;
+        private long beginDate;
+        private long endDate;
+        private long timePromotion;
+
+        private int status;
+        private float finalPrice;
+        private float quantityAdult;
+        private float quantityBaby;
+        private float minCapacity;
+        private float quantityChild;
+
+        private string idTour;
+        private string nameTour;
+
+        private Guid idCar;
+        private string liscensePlate;
+        private string nameDriver;
+
+        private Guid idEmployee;
+        private string nameEmployee;
+
+        private int idPromotion;
+
+        private ICollection<Promotion> promotions;
+        private ICollection<Timeline> timelines;
+
+        public string Id { get => id; set => id = value; }
+        public long DepartureDate { get => departureDate; set => departureDate = value; }
+        public long BeginDate { get => beginDate; set => beginDate = value; }
+        public long EndDate { get => endDate; set => endDate = value; }
+        public long TimePromotion { get => timePromotion; set => timePromotion = value; }
+        public int Status { get => status; set => status = value; }
+        public float FinalPrice { get => finalPrice; set => finalPrice = value; }
+        public float QuantityAdult { get => quantityAdult; set => quantityAdult = value; }
+        public float QuantityBaby { get => quantityBaby; set => quantityBaby = value; }
+        public float MinCapacity { get => minCapacity; set => minCapacity = value; }
+        public float QuantityChild { get => quantityChild; set => quantityChild = value; }
+        public string IdTour { get => idTour; set => idTour = value; }
+        public string NameTour { get => nameTour; set => nameTour = value; }
+        public Guid IdCar { get => idCar; set => idCar = value; }
+        public string LiscensePlate { get => liscensePlate; set => liscensePlate = value; }
+        public Guid IdEmployee { get => idEmployee; set => idEmployee = value; }
+        public string NameEmployee { get => nameEmployee; set => nameEmployee = value; }
+        public string NameDriver { get => nameDriver; set => nameDriver = value; }
+        public ICollection<Promotion> Promotions { get => promotions; set => promotions = value; }
+        public ICollection<Timeline> Timelines { get => timelines; set => timelines = value; }
+        public int IdPromotion { get => idPromotion; set => idPromotion = value; }
+    }
+}
