6c53827699d56918375b1bf94d04835cdf7c1be5
Long.nh <ng.hglong102@gmail.com>
2022-10-08 13:27:48 +0700

    fix data

--- Travel.Shared/ViewModels/Responsive.cs
+++ Travel.Shared/ViewModels/Responsive.cs
@@ -8,7 +8,7 @@ namespace Travel.Shared.ViewModels
 {
     public class Response: Pagination
     {
-        public string Content { get; set; }
+        public object Content { get; set; }
         public int TotalResult { get; set; }
         public Notification Notification { get; set; } = new Notification();
         
