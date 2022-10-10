391eb829d7c5433b4a277ada4576b4ba23fc4890
Long.nh <ng.hglong102@gmail.com>
2022-10-11 06:24:03 +0700

    signalR client test

--- src/app/pages/Location/province/list-province/list-province.component.ts
+++ src/app/pages/Location/province/list-province/list-province.component.ts
@@ -5,6 +5,8 @@ import { ProvinceService } from 'src/app/services_API/province.service';
 import { LocationModel } from "../../../../models/location.model";
 import { ResponseModel } from "../../../../models/responsiveModels/response.model";
 
+// signalr
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 
 @Component({
   selector: 'app-list-province',
@@ -17,10 +19,22 @@ export class ListProvinceComponent implements OnInit {
   response: ResponseModel
   child: LocationModel
   type: string
+
+  //signalr 
+  private hubConnectionBuilder!: HubConnection;
+  offers : any[] = [];
+
   constructor(private provinceService: ProvinceService, private notificationService: NotificationService,
      private configService: ConfigService){}
   ngOnInit(): void {
+    this.hubConnectionBuilder = new HubConnectionBuilder().withUrl('https://localhost:44394/travelhub').configureLogging(LogLevel.Information).build();
+    this.hubConnectionBuilder.start().then(()=> console.log("Da ket noi")).catch(err => console.log("error"));
+    this.hubConnectionBuilder.on('SendOffersToUser', (result: any) => {
+      this.offers.push(result);
+    })
+
     this.provinceService.GetProvince().subscribe(res => {
+
       this.response = res
 
       if(this.response.notification.type == "Error")
@@ -29,7 +43,8 @@ export class ListProvinceComponent implements OnInit {
       }
 
       this.resProvince = this.response.content
-      console.log(this.response.content);
+
+      console.log("finisheed");
 
     })
   }
@@ -38,5 +53,8 @@ export class ListProvinceComponent implements OnInit {
     this.child = data
     this.type = type
   }
-
+  clickbtn(){
+    this.provinceService.GetData().subscribe(res => {
+    })
+  };
 }
