abc8e7dcb071f16edb70b60b55bfa345843a6ae3
Dev_HuyHoa <driverhuyoa@gmail.com>
2022-10-11 18:02:17 +0700

    banner

--- src/app/pages/banner/banner.component.ts
+++ src/app/pages/banner/banner.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { BannerModel } from 'src/app/models/banner.model';
+import { ResponseModel } from 'src/app/models/responsiveModels/response.model';
 import { BannerService } from "../../services_API/banner.service";
+import { NotificationService } from "../../services_API/notification.service";
+import { ConfigService } from "../../services_API/config.service";
 
 @Component({
   selector: 'app-banner',
@@ -7,13 +11,33 @@ import { BannerService } from "../../services_API/banner.service";
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
-
-  constructor(private _bannerService: BannerService) {  }
+  constructor(private _bannerService: BannerService, private notificationService: NotificationService,
+    private configService: ConfigService) {  }
   files: any
   nameBanner: string
-  ngOnInit(): void {
-  }
 
+  resBanner: BannerModel[]
+  response: ResponseModel
+  type: string
+
+  ngOnInit(): void {
+    this._bannerService.gets().subscribe(res => {
+    this.response = res
+    if(!this.response.notification.type)
+    {
+      this.resBanner = this.response.content
+      console.log(this.resBanner);
+
+    }
+    else{
+      this.notificationService.handleAlertObj(res.notification)
+    }
+  }, error => {
+    var message = this.configService.error(error.status, error.error != null?error.error.text:"");
+    this.notificationService.handleAlert(message, "Error")
+  })
+
+}
   changeImg(e){
     this.files = e
   }
