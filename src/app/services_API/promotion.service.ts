b4547e3f80385f28b17ec661013903fba15ad216
hienpt9602@gmail.com <hienpt9602@gmail.com>
2022-10-17 11:42:16 +0700

    fix promotion

--- src/app/services_API/promotion.service.ts
+++ src/app/services_API/promotion.service.ts
@@ -2,14 +2,37 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { ConfigService } from "./config.service";
 import { ResponseModel } from "../models/responsiveModels/response.model";
+import { PromotionModel } from "../models/promotion.model";
+import { NotificationService } from "../services_API/notification.service"
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class PromotionService{
-  constructor(private http:HttpClient, private configService:ConfigService){ }
+  constructor(private http:HttpClient, private configService:ConfigService,private notificationService: NotificationService){ }
+  response: ResponseModel
+  resPromotion: PromotionModel[]
+  async views()
+  {
+    var value = <any>await new Promise<any>(resolve => {
+      this.http.get<ResponseModel>( this.configService.apiUrl + "/api/Promotion/gets-promotion").subscribe(res => {
+        this.response = res
+        if(!this.response.notification.type)
+        {
+          this.resPromotion =  this.response.content
+          resolve(this.resPromotion);
+        }
+        else{
+          this.notificationService.handleAlertObj(res.notification)
+        }
+    }, error => {
+      var message = this.configService.error(error.status, error.error != null?error.error.text:"");
+      this.notificationService.handleAlert(message, "Error")
+    })})
+    return value
 
+  }
   gets()
   {
       return this.http.get<ResponseModel>( this.configService.apiUrl + "/api/Promotion/gets-promotion");
