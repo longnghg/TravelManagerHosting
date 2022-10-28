ad28b352c6c40e7ef1f2e081460516627c23f65a
KIET\Administrator <phuongkiet850@gmail.com>
2022-10-28 12:58:07 +0700

    check thời gian login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AuthenticationModel } from "./models/authentication.model";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,9 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'argon-dashboard-angular';
-
+  resAthentication: AuthenticationModel
   ngOnInit(): void {
+    this.resAthentication = JSON.parse(localStorage.getItem("currentUser"))
+    if(this.resAthentication){
+      var startDay = new Date();
+      var endDay = new Date(this.resAthentication.dateTime)
 
+      var millisBetween = startDay.getTime() - endDay.getTime();
+
+      var days = millisBetween / (1000 * 3600 * 24);
+
+      if (Math.round(Math.abs(days)) == 1) {
+        localStorage.removeItem("currentUser")
+        localStorage.removeItem("idUser")
+        localStorage.removeItem("token")
+        sessionStorage.clear()
+      }
+
+    }
 
   }
 }
